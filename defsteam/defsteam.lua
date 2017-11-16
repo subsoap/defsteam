e6cddbcb714d5e50cf936ffa0ac57a9e0b5b7966
Brian Kramer <subsoap@gmail.com>
2017-11-16 13:48:41 -0800

    More setup

--- defsteam/defsteam.lua
+++ defsteam/defsteam.lua
@@ -1,1 +1,21 @@
---
+local M = {}
+
+function M.init()
+	local ok, err = pcall(function()
+		local steamworks = require("defsteam.steamworks")
+		M.active = true
+		for k,v in pairs(steamworks) do
+			if not M[k] then
+				M[k] = v
+			end
+		end
+	end)
+
+	if not ok then
+		print("DefSteam: Steamworks couldn't be started. Is Steam running?")
+		M.active = false
+	end	
+end
+
+
+return M
